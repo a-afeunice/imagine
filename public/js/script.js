4bf23135f2ff0478a80baf44b5e8736da6f62109
Eunice Afrifa <afrifae2@gmail.com>
2024-06-12 12:16:31 +0000

    Objects in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -205,6 +205,45 @@ numbers.filter (even);
 
 // Array methods in JavaScript
 
+// Write a function that takes a user with name and dateOfBirth and returns true if their birthday is today else false
+// const user = {
+//   name: 'Martin King',
+//   dateOfBirth: '1990-06-12'
+// }
+function isBirthday(user) {
+  const today = new Date();
+  const birthDate = new Date(user.dateOfBirth);
+  // console.log(birthDate.getMonth(), today.getMonth());
+  // console.log(birthDate.getDate(), today.getDate());
+  if (today.getMonth() === birthDate.getMonth() && today.getDate() === birthDate.getDate()) {
+    return true;
+  } else {
+    return false;
+  }
+    
+}
+
+
+isBirthday(user);
+
+
+// Write a function that takes a user with name and dateOfBirth and returns birthday with their name if their birthday is today else return not your day
+
+function birthdayMessage(user) {
+   if (isBirthday(user)) {
+  return "happy birthday Martin!";
+ } else {
+   return 'not your day';
+ }
+ }
+
+birthdayMessage(user);
+
+
+
+
+
+
 
 
 
